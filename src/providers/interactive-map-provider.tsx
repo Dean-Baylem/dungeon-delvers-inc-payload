18a0fc05e906f0822dae159d3fcda9947ede8de5
Dean-Baylem <dbaylem5822@gmail.com>
2026-01-31 17:45:13 +0900

    Commented out files related to interactive map as not implemented yet

--- src/providers/interactive-map-provider.tsx
+++ src/providers/interactive-map-provider.tsx
@@ -1,41 +1,41 @@
-'use client';
-import { type ReactNode, createContext, useContext, useRef } from "react";
-import { useStore } from "zustand";
-import { type InteractiveMapStore, createInteractiveMapStore } from "@/stores/interactive-map-store";
+// 'use client';
+// import { type ReactNode, createContext, useContext, useRef } from "react";
+// import { useStore } from "zustand";
+// import { type InteractiveMapStore, createInteractiveMapStore } from "@/stores/interactive-map-store";
 
-export type InteractiveMapStoreApi = ReturnType<typeof createInteractiveMapStore>
+// export type InteractiveMapStoreApi = ReturnType<typeof createInteractiveMapStore>
 
-export const InteractiveMapContext = createContext<InteractiveMapStoreApi | undefined>(
-    undefined,
-);
+// export const InteractiveMapContext = createContext<InteractiveMapStoreApi | undefined>(
+//     undefined,
+// );
 
-export interface InteractiveMapProviderProps {
-    children: ReactNode
-}
+// export interface InteractiveMapProviderProps {
+//     children: ReactNode
+// }
 
-export const InteractiveMapProvider = ({
-    children,
-}: InteractiveMapProviderProps) => {
-    const mapRef = useRef<InteractiveMapStoreApi | null>(null);
-    if (mapRef.current === null) {
-        mapRef.current = createInteractiveMapStore();
-    }
+// export const InteractiveMapProvider = ({
+//     children,
+// }: InteractiveMapProviderProps) => {
+//     const mapRef = useRef<InteractiveMapStoreApi | null>(null);
+//     if (mapRef.current === null) {
+//         mapRef.current = createInteractiveMapStore();
+//     }
 
-    return (
-        <InteractiveMapContext.Provider value={mapRef.current}>
-            {children}
-        </InteractiveMapContext.Provider>
-    )
-}
+//     return (
+//         <InteractiveMapContext.Provider value={mapRef.current}>
+//             {children}
+//         </InteractiveMapContext.Provider>
+//     )
+// }
 
-export const useInteractiveMapStore = <T,>(
-    selector: (map: InteractiveMapStore) => T,
-): T => {
-    const interactiveMapContext = useContext(InteractiveMapContext);
+// export const useInteractiveMapStore = <T,>(
+//     selector: (map: InteractiveMapStore) => T,
+// ): T => {
+//     const interactiveMapContext = useContext(InteractiveMapContext);
 
-    if (!interactiveMapContext) {
-        throw new Error('useInteractiveMapStore must be used within InteractiveMapProvider');
-    }
+//     if (!interactiveMapContext) {
+//         throw new Error('useInteractiveMapStore must be used within InteractiveMapProvider');
+//     }
 
-    return useStore(interactiveMapContext, selector);
-}
+//     return useStore(interactiveMapContext, selector);
+// }
--- src/stores/interactive-map-store.ts
+++ src/stores/interactive-map-store.ts
@@ -1,96 +1,96 @@
-import { createStore } from 'zustand';
-import { fetcher } from '@/lib/api/fetcher';
-import { FilteredMapRegion } from '@/types/filteredMapRegion';
-import { FilteredMapIcons } from '@/types/filteredMapIcons';
+// import { createStore } from 'zustand';
+// import { fetcher } from '@/lib/api/fetcher';
+// import { FilteredMapRegion } from '@/types/filteredMapRegion';
+// import { FilteredMapIcons } from '@/types/filteredMapIcons';
 
-export type MapState = {
-  isNewIcon: boolean;
-  showRegionForm: boolean;
-  displayMode: string;
-  disabledInteraction: boolean;
-  newRegionCoordinates: [number, number][];
-  newIconPos: { xPoint: number; yPoint: number };
-  newIconForm: boolean;
-  storedMapIcons: Array<{
-    name: string;
-    xPoint: number;
-    yPoint: number;
-    pinType: string;
-    pinSummary: string;
-    linkedMap: number;
-    id: number
-  }>;
-  allMapRegions: FilteredMapRegion[];
-  editRegion: { id: number; coordinates: Array<{ xPoint: number; yPoint: number }> };
-};
+// export type MapState = {
+//   isNewIcon: boolean;
+//   showRegionForm: boolean;
+//   displayMode: string;
+//   disabledInteraction: boolean;
+//   newRegionCoordinates: [number, number][];
+//   newIconPos: { xPoint: number; yPoint: number };
+//   newIconForm: boolean;
+//   storedMapIcons: Array<{
+//     name: string;
+//     xPoint: number;
+//     yPoint: number;
+//     pinType: string;
+//     pinSummary: string;
+//     linkedMap: number;
+//     id: number
+//   }>;
+//   allMapRegions: FilteredMapRegion[];
+//   editRegion: { id: number; coordinates: Array<{ xPoint: number; yPoint: number }> };
+// };
 
-export type MapActions = {
-  handleSetAllMapRegionData: (newData: FilteredMapRegion[]) => void;
-  handleRegionDisplayToggle: () => void;
-  handleMapStateChange: (newState: string) => void;
-  handleShowRegionForm: (newState: boolean) => void;
-  handleCancelNewPinForm: () => void;
-  handleSetNewRegionCoordinates: (newPolygon: [number, number][]) => void;
-  handleEditRegionState: (newState: {
-    id: number;
-    coordinates: Array<{ xPoint: number; yPoint: number }>;
-  }) => void;
-  handleNewIconPosState: (newState: { xPoint: number; yPoint: number }) => void;
-  handleNewIconFormState: (newState: boolean) => void;
-  handleUpdateStoredMapIcons: (
-    newState: Array<{
-      name: string;
-      xPoint: number;
-      yPoint: number;
-      pinType: string;
-      pinSummary: string;
-      linkedMap: number;
-      id: number
-    }>,
-  ) => void;
-};
+// export type MapActions = {
+//   handleSetAllMapRegionData: (newData: FilteredMapRegion[]) => void;
+//   handleRegionDisplayToggle: () => void;
+//   handleMapStateChange: (newState: string) => void;
+//   handleShowRegionForm: (newState: boolean) => void;
+//   handleCancelNewPinForm: () => void;
+//   handleSetNewRegionCoordinates: (newPolygon: [number, number][]) => void;
+//   handleEditRegionState: (newState: {
+//     id: number;
+//     coordinates: Array<{ xPoint: number; yPoint: number }>;
+//   }) => void;
+//   handleNewIconPosState: (newState: { xPoint: number; yPoint: number }) => void;
+//   handleNewIconFormState: (newState: boolean) => void;
+//   handleUpdateStoredMapIcons: (
+//     newState: Array<{
+//       name: string;
+//       xPoint: number;
+//       yPoint: number;
+//       pinType: string;
+//       pinSummary: string;
+//       linkedMap: number;
+//       id: number
+//     }>,
+//   ) => void;
+// };
 
-export type InteractiveMapStore = MapState & MapActions;
+// export type InteractiveMapStore = MapState & MapActions;
 
-export const defaultInitState: MapState = {
-  showRegionForm: false,
-  displayMode: '',
-  isNewIcon: false,
-  disabledInteraction: false,
-  newRegionCoordinates: [],
-  newIconPos: { xPoint: 0, yPoint: 0 },
-  newIconForm: false,
-  allMapRegions: [],
-  storedMapIcons: [],
-  editRegion: { id: 0, coordinates: [{ xPoint: 0, yPoint: 0 }] },
-};
+// export const defaultInitState: MapState = {
+//   showRegionForm: false,
+//   displayMode: '',
+//   isNewIcon: false,
+//   disabledInteraction: false,
+//   newRegionCoordinates: [],
+//   newIconPos: { xPoint: 0, yPoint: 0 },
+//   newIconForm: false,
+//   allMapRegions: [],
+//   storedMapIcons: [],
+//   editRegion: { id: 0, coordinates: [{ xPoint: 0, yPoint: 0 }] },
+// };
 
-export const createInteractiveMapStore = (initState: MapState = defaultInitState) => {
-  return createStore<InteractiveMapStore>()((set) => ({
-    ...initState,
-    handleMapStateChange: (newState) =>
-      set((state) => ({
-        displayMode: state.displayMode === newState ? '' : newState,
-        newRegionCoordinates: [],
-        editRegion: { id: 0, coordinates: [] },
-      })),
-    handleRegionDisplayToggle: () =>
-      set((state) => ({
-        displayMode: state.displayMode === 'showRegions' ? '' : 'showRegions',
-      })),
-    handleSetNewRegionCoordinates: (newPolygon) =>
-      set((state) => ({
-        newRegionCoordinates: newPolygon,
-      })),
-    handleCancelNewPinForm: () => set(() => ({ isNewIcon: false, disabledInteraction: false })),
-    handleSetAllMapRegionData: (newData) =>
-      set(() => ({
-        allMapRegions: newData,
-      })),
-    handleShowRegionForm: (newState) => set(() => ({ showRegionForm: newState })),
-    handleEditRegionState: (newState) => set(() => ({ editRegion: newState })),
-    handleNewIconFormState: (newState) => set(() => ({ newIconForm: newState })),
-    handleNewIconPosState: (newState) => set(() => ({ newIconPos: newState })),
-    handleUpdateStoredMapIcons: (newState) => set(() => ({ storedMapIcons: newState })),
-  }));
-};
+// export const createInteractiveMapStore = (initState: MapState = defaultInitState) => {
+//   return createStore<InteractiveMapStore>()((set) => ({
+//     ...initState,
+//     handleMapStateChange: (newState) =>
+//       set((state) => ({
+//         displayMode: state.displayMode === newState ? '' : newState,
+//         newRegionCoordinates: [],
+//         editRegion: { id: 0, coordinates: [] },
+//       })),
+//     handleRegionDisplayToggle: () =>
+//       set((state) => ({
+//         displayMode: state.displayMode === 'showRegions' ? '' : 'showRegions',
+//       })),
+//     handleSetNewRegionCoordinates: (newPolygon) =>
+//       set((state) => ({
+//         newRegionCoordinates: newPolygon,
+//       })),
+//     handleCancelNewPinForm: () => set(() => ({ isNewIcon: false, disabledInteraction: false })),
+//     handleSetAllMapRegionData: (newData) =>
+//       set(() => ({
+//         allMapRegions: newData,
+//       })),
+//     handleShowRegionForm: (newState) => set(() => ({ showRegionForm: newState })),
+//     handleEditRegionState: (newState) => set(() => ({ editRegion: newState })),
+//     handleNewIconFormState: (newState) => set(() => ({ newIconForm: newState })),
+//     handleNewIconPosState: (newState) => set(() => ({ newIconPos: newState })),
+//     handleUpdateStoredMapIcons: (newState) => set(() => ({ storedMapIcons: newState })),
+//   }));
+// };
