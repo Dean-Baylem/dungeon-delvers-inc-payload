5f984bbdff436c1d6c39239670b4077739943ced
Dean Baylem <dbaylem5822@gmail.com>
2026-03-18 10:02:27 +0900

    Started development on location map page

--- src/lib/mappers/gazetteerCardMapper.ts
+++ src/lib/mappers/gazetteerCardMapper.ts
@@ -2,7 +2,7 @@ import { GazetteerCardType } from '@/types/gazetteerCard/gazetteerCardType';
 
 export const mapLocationToGazetteerCard = (gazeteerItem: any): GazetteerCardType => ({
   name: gazeteerItem.name,
-  slug: gazeteerItem.slug,
+  slug: gazeteerItem.pageSlug,
   type: gazeteerItem.type,
   terrain: gazeteerItem.terrain,
   summary: gazeteerItem.summary,
